3de56a3c957bd877deaa73c66f5cd3f71459597a
eesaasheikh <eesaasheikh3@gmail.com>
2023-01-31 14:35:03 +0000

    mad a carousel function that shows different images. Not working yet.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,40 @@
+import noodleGirl from './imageJar/NoodleSoup.png'
+import chef from './imageJar/chef.png'
+import noodleFood from './imageJar/food.png'
+
 const Home = () => {
     return ( 
         <>
-        <h1> this is home page</h1>
-        <p>
-            fabhjrbaeklbrvjekbvjekvbevvebvebr
-        </p>
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
 
-        <p>
-            ajhvbfrlvgkbferkvaejkfbvjefbv
-        </p>
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src={noodleGirl} alt="Eating Noodles"/>
+    </div>
+
+    <div class="item">
+      <img src={noodleFood} alt="Chicago"/>
+    </div>
+
+    <div class="item">
+      <img src={chef} alt="New York"/>
+    </div>
+  </div>
+
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>    
         </>
      );
 }
